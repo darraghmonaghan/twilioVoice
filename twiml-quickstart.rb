627b3191a55f3d40c2a7f65e46e9c5edda74e833
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-04-13 08:41:30 +0100

    customized greeting

--- twiml-quickstart.rb
+++ twiml-quickstart.rb
@@ -4,8 +4,15 @@ require 'sinatra'
 require 'twilio-ruby'
  
 get '/hello-monkey' do
+
+  people = {
+  		'+442890600191' => 'Monaghan Household',
+  }
+
+  name = people(params['From']) || 'There'
+
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hi There, Please wait one moment whilst we connect you to Darragh'
+    r.Say 'Hi #{name}, Please wait one moment whilst we connect you to Darragh. He is a busy guy, let me try and find him.'
     r.Dial '+442890600191'
   end.text
 end
